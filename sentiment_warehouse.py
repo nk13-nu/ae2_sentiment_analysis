b6ebb32e72b971da8382ceb985663c9922ba76d2
Nicolas <nkpv327@gmail.com>
2026-04-07 12:26:08 +0100

    added most methods in the sentiment warehouse class, still need to add calculate average sentiment

--- sentiment_warehouse.py
+++ sentiment_warehouse.py
@@ -3,6 +3,20 @@ In this script we should define the non-persistent storage class - an object tha
 stores the responses made to the current server which has a method to calculate
 and return the average responses and is accessible from an endpoint.
 """
+from typing import List, Dict
 
 class SentimentWarehouse:
-    pass
+    def __init__(self, sentiments: List[Dict] = None):
+        if sentiments is not None:
+          self.sentiments = sentiments
+        else:
+            self.sentiments = []
+
+    def store(self, sentiments):
+        self.sentiments.append(sentiments)
+
+    def get_all_sentiments(self):
+        return self.sentiments
+    
+    def calculate_average_sentiment(self):
+        pass

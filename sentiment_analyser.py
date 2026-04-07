2c8283cd9cca5de376fca09a0defb99940e5315d
HJFS1 <jfarquharsmith@gmail.com>
2026-04-07 11:31:56 +0100

    Finished sentiment_analyser.py
    Implement the Sentiment Analysis Class here using HuggingFace's pipeline and through the "text-classification" model accessed on "transformersbook/distilbert-base-uncased-finetuned-emotion

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -1,5 +1,20 @@
 """Implement the Sentiment Analysis Class here using HuggingFace's pipeline 
 and through the "text-classification" model accessed on "transformersbook/distilbert-base-uncased-finetuned-emotion"""
 
+from transformers import pipeline
+
+sentiment_pipeline = pipeline("text-classification",model="transformersbook/distilbert-base-uncased-finetuned-emotion")
+
 class SentimentAnalyser:
-    pass
+    def _init_(self, text:str):
+        if not isinstance(text, str):
+            raise TypeError(f"{text} must be a String")
+        self.text = text
+
+    def get_sentiment(self):
+        emotion_predictions = sentiment_pipeline(self.text, top_k=None)
+        
+        return {
+            "text": self.text,
+            "labels": emotion_predictions
+        }

019cf5aa4879bb8f0a0fc378ee2530d217f0a3d9
nk13-nu <kuriperezvillaman.n@northeastern.edu>
2026-04-09 10:47:29 +0100

    Update sentiment_analyser.py
    There was an error in the constructor, but it is fixed now.

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -6,7 +6,7 @@ from transformers import pipeline
 sentiment_pipeline = pipeline("text-classification",model="transformersbook/distilbert-base-uncased-finetuned-emotion")
 
 class SentimentAnalyser:
-    def _init_(self, text:str):
+    def __init__(self, text:str):
         if not isinstance(text, str):
             raise TypeError(f"{text} must be a String")
         self.text = text

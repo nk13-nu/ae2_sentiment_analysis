b73a556b9657f5fa4d21e1e0004fbf923912d08a
Nicolas <nkpv327@gmail.com>
2026-04-09 10:23:14 +0100

    added tags to endpoint parameters to improve documentation readability

--- main.py
+++ main.py
@@ -15,21 +15,27 @@ app = FastAPI(title= "Customer Feedback Sentiment Analysis API",
 
 sentiment_warehouse = SentimentWarehouse()
 
-@app.get("/")
+@app.get("/", tags=["Main Page"])
 def root():
     return {"message": "Welcome to the Sentiment Analysis API. This API is intended for organisations to classify user feedback across six emotions and improve products and customer service."}
 
 @app.get("/sentiment/{text}", summary="Input a product or service review and get a sentiment score across six emotions", 
-         description="This endpoint takes a product review as text and returns six different scores across six emotions.", response_model=APIResponse)
+         description="This endpoint takes a product review as text and returns six different scores across six emotions.", 
+         response_model=APIResponse,
+         tags=["Sentiment Analysis (Across Six Emotions)"])
 def analyse_sentiment(text:str):
     prediction = SentimentAnalyser(text=text).get_sentiment()
     sentiment_warehouse.store(prediction)
     return prediction
 
-@app.get("/analyse/allSentiments", summary="Retrieve all sentiments for each piece of text in this session.")
+@app.get("/analyse/allSentiments",
+         summary="Retrieve all sentiments for each piece of text in this session.",
+         tags=["Analytics"])
 def all_sentiments():
     return sentiment_warehouse.get_all_sentiments()
 
-@app.get("/analyse/averageSentiment", summary="Retrieve the average sentiment per emotion across all current product reviews.")
+@app.get("/analyse/averageSentiment",
+         summary="Retrieve the average sentiment per emotion across all current product reviews.",
+         tags=["Analytics"])
 def average_sentiment():
     return sentiment_warehouse.calculate_average_sentiment()
